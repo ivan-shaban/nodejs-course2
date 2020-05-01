39e7532cb4c594144782aaa11dda03f9d5abc633
ishaban <ishaban.flash@gmail.com>
2020-05-02 00:20:51 +0300

    Move authentication logic to authentication service.

--- src/common/authentication.ts
+++ src/common/authentication.ts
@@ -1,21 +1,15 @@
-import { promisify } from 'util'
-
 import * as express from 'express'
-import { verify } from 'jsonwebtoken'
 import createError from 'http-errors'
 
-import { JWTPayload } from '../resources/login/service'
-
-import { JWT_SECRET_KEY } from './config'
-
-const verifyJWT = promisify(verify)
+import { LoginService } from '../resources/login/service'
+import { iocContainer } from '../di/inversify.config'
 
 export async function expressAuthentication(request: express.Request, securityName: string, scopes?: string[]) {
     if (securityName === 'jwt') {
         const token = request.headers.authorization
         if (token) {
-            const bearerToken = token.split(' ')[1]
-            const jwtPayload = await verifyJWT(bearerToken, JWT_SECRET_KEY) as JWTPayload
+            const loginService = iocContainer.get(LoginService)
+            const jwtPayload = loginService.verifyJWT(token)
 
             return jwtPayload
         }
--- src/resources/login/service.ts
+++ src/resources/login/service.ts
@@ -1,5 +1,8 @@
+import { promisify } from 'util'
+
 import jwt from 'jsonwebtoken'
 import { injectable } from 'inversify'
+import { verify } from 'jsonwebtoken'
 
 import { JWT_SECRET_KEY } from '../../common/config'
 
@@ -8,13 +11,15 @@ export interface JWTPayload {
     readonly login: string;
 }
 
+const verifyJWT = promisify(verify)
+
 @injectable()
 export class LoginService {
-    public verifyJWT = (userId: string, login: string) => {
-        const payload: JWTPayload = { userId, login }
-        const token = jwt.sign(payload, JWT_SECRET_KEY)
+    public verifyJWT = async (token: string) => {
+        const bearerToken = token.split(' ')[1]
+        const payload = await verifyJWT(bearerToken, JWT_SECRET_KEY) as JWTPayload
 
-        return token
+        return payload
     }
 
     public createJWT = (userId: string, login: string) => {
